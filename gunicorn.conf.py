85267cfa5368bf82c8ad3ff990392b2e057e71ad
Ben Isenstein <ben.isenstein@gmail.com>
2023-10-05 14:24:11 -0600

    feat: gunicorn config file for more options, logging

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+import os
+
+bind = f'[::]:{os.environ.get("PORT") if "PORT" in os.environ else 8080}'
+
+wsgi_app = 'main:app'
+
+accesslog = '-'
+
+capture_output = True
+
+proxy_allow_ips = '*'
+
+timeout = 300
