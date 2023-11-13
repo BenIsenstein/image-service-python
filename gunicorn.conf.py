ef14272dcefd6bbcc5808142506dfa2cb6dc5df2
Ben Isenstein <ben.isenstein@gmail.com>
2023-11-12 22:13:14 -0700

    feat: gunicorn config from env vars

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,4 +10,10 @@ capture_output = True
 
 proxy_allow_ips = '*'
 
-timeout = 0
+timeout = int(os.environ.get("TIMEOUT")) if "TIMEOUT" in os.environ else 0
+
+workers = int(os.environ.get("WORKERS")) if "WORKERS" in os.environ else 1
+
+print('bind: ', bind)
+print('timeout: ', timeout)
+print('workers: ', workers)
